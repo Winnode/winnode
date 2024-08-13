45749132ac5691bd77a372ef8926418199bf82b1
WinNode <maladarsih@gmail.com>
2024-08-14 01:39:54 +0700

    Update Initial.sh

--- Initial.sh
+++ Initial.sh
@@ -1,7 +1,9 @@
-GREEN="\e[1m\e[1;32m" # green color
-RED="\e[1m\e[1;31m" # red color
-BLUE='\033[0;34m'   # blue color
-NC="\e[0m"          # no color
+#!/bin/bash
+
+GREEN="\e[1m\e[1;32m"
+RED="\e[1m\e[1;31m"
+BLUE='\033[0;34m'
+NC="\e[0m"
 
 function printLogo {
   bash <(curl -s https://raw.githubusercontent.com/Winnode/winnode/main/Logo.sh)
@@ -10,7 +12,6 @@ function printLogo {
 function printLine {
   echo -e "${BLUE}════════════════════════════════════════════════════════════════════════════════${NC}"
 }
-
 
 function printGreen {
   echo -e "${GREEN}${1}${NC}"
@@ -26,8 +27,17 @@ function printBlue {
 
 function addToPath {
   source $HOME/.bash_profile
-  PATH_EXIST=$(grep "${1}" $HOME/.bash_profile)
-  if [ -z "$PATH_EXIST" ]; then
+  if ! grep -q "${1}" $HOME/.bash_profile; then
     echo "export PATH=\$PATH:${1}" >> $HOME/.bash_profile
+    printGreen "Added ${1} to PATH."
+  else
+    printBlue "${1} is already in PATH."
   fi
 }
+
+printLogo
+printLine
+printGreen "This is green text."
+printRed "This is red text."
+printBlue "This is blue text."
+addToPath "/new/path"
